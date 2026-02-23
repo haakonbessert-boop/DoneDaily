4049568710c6f3a9e950d76f41441e0816368e42
i589273 <bdhkjqwdvkw@kjdkw>
2026-02-23 10:08:34 +0100

    Initial Commit

---
+++ DoneDaily/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DoneDaily
+//
+//  Created by Bessert,Haakon on 23.02.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DoneDaily/DoneDailyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoneDailyApp.swift
+//  DoneDaily
+//
+//  Created by Bessert,Haakon on 23.02.26.
+//
+
+import SwiftUI
+
+@main
+struct DoneDailyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
